56fe215c4db0fd71f1f4fcde913d4fcee9e026bd
aya <katoa5429@gmail.com>
2022-12-27 00:48:52 +0900

    新規登録画面とログイン画面のview作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
