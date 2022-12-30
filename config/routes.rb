9331b056cbb92c86ffa8f7e1b930579973496a06
aya <katoa5429@gmail.com>
2022-12-30 19:30:08 +0900

    ヘッダータイトル編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root 'pages#index'
-  get 'pages/show'
+  root 'static_pages#top'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
   resources :users, only: %i[new create]
+  resources :calendars, only: %i[index]
 end
