6a854a8fac2387d372954ddc510524beca0cd7f8
aya <katoa5429@gmail.com>
2023-01-01 23:16:34 +0900

    プロフィール作成

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,24 @@
+class ProfilesController < ApplicationController
+  before_action :set_user, only: %i[update edit]
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to profile_path, success: t('defaults.message.updated', item: User.model_name.human)
+    else
+      flash.now['danger'] = t('defaults.message.not_updated', item: User.model_name.human)
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+end
