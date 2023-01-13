baefd9e695a7fbbb940593b6861de1e2194e197d
aya <katoa5429@gmail.com>
2023-01-13 17:25:34 +0900

    習慣の登録画面編集

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,3 +1,25 @@
 class HabitsController < ApplicationController
-  
+  def index
+    @habits = Habit.all.includes(:user).order(created_at: :desc)
+  end
+
+  def new
+    @habit = Habit.new
+  end
+
+  def create
+    @habit = current_user.habits.build(habit_params)
+    if @habit.save
+      redirect_to calendars_path, success: t('defaults.message.created', item: Habit.model_name.human)
+    else
+      flash.now['danger'] = t('defaults.message.not_created', item: Habit.model_name.human)
+      render :new
+    end
+  end
+
+  private
+
+  def habit_params
+    params.require(:habit).permit(:title, :body, :start_day, :day_of_week, :habit_type, :total_number, :start_number, :end_number, :target_number, :url)
+  end
 end
