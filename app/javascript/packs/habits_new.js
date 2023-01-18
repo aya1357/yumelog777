3aa0758070ce13f5cb8d446c4e9f1434f90902a7
aya <katoa5429@gmail.com>
2023-01-18 22:33:17 +0900

    習慣の新規登録画面の曜日のクリックイベントをf.hidden_fieldのvalue値に送る実装作成

--- app/javascript/packs/habits_new.js
+++ app/javascript/packs/habits_new.js
@@ -1,19 +1,67 @@
 window.addEventListener('DOMContentLoaded', function(){
-  /** jQueryの処理 */
-  // $("#button1").click(function(){
-  //   const str1 = $("#hidden1").val();
-  //   $("#span1").text(str1);
-  // });
-  $("#dayOfWeek0").click(function(){
-    $(this).toggleClass("hid bg-gray-50 text-blue-700 bg-blue-600 text-slate-50");
-    console.log($(this).hasClass("hid"));
+  $(".dayOfWeek").click(function(){
+    let week_value = check_week_value($(this).val());
+
+    $(this).toggleClass("hid bg-gray-50 text-blue-700 bg-blue-700 text-slate-50");
+
     if ($(this).hasClass("hid")) {
-      $("#hidden2").val("0");
-      $("#span1").text($("#hidden2").val(""));
+      let current_value = $("#week_hidden").val();
+      // クリックしてclassが存在するとき
+      if (current_value != '')
+      {
+        // 値が設定されている場合(ex.0がhidden_fieldに設定済みの場合. [0] ⇨ [0,1] ⇨ 0,1)
+        let split_value = current_value.split(',');
+        split_value.push(week_value);
+        let new_value = split_value.join(',');
+        $("#week_hidden").val(new_value);
+      } else {
+        // 値が設定されていない場合
+        $("#week_hidden").val(week_value);
+      }
     } else {
-      $("#hidden2").val("");
-      $("#span1").text("");
+      let current_value = $("#week_hidden").val();
+      if (current_value != '')
+      // 値が存在する時
+      {
+        let split_value = current_value.split(',');
+        if (split_value.includes(week_value)) {
+          let index = split_value.indexOf(week_value);
+          split_value.splice(index, 1);
+          let new_value = split_value.join(',');
+          $("#week_hidden").val(new_value);
+        }
+      }
     };
   });
 
+  function check_week_value(checked_week_value) {
+    switch (checked_week_value) {
+      case "日":
+        week_value = '0';
+        break;
+      case "月":
+        week_value = '1';
+        break;
+      case "火":
+        week_value = '2';
+        break;
+      case "水":
+        week_value = '3';
+        break;
+      case "木":
+        week_value = '4';
+        break;
+      case "金":
+        week_value = '5';
+        break;
+      case "土":
+        week_value = '6';
+        break;
+      default:
+        week_value = '';
+        break;
+    }
+
+    return week_value;
+  }
 });
