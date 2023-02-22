12b7f36bb7424b1d55ac5681c14e443c37b29cbb
aya <katoa5429@gmail.com>
2023-02-22 23:42:26 +0900

    ログの新規登録画面で、勉強したページ数と計算の箇所に勉強のidを付与

---
+++ app/javascript/packs/logs_new.js
@@ -0,0 +1,10 @@
+document.addEventListener("turbo:load", function () {
+  // $('div[id^="study_"]').click(function (e) {
+  $('#study_1').click(function () {
+    // e.preventDefault();
+    console.log("テスト");
+    // let studypages = $(this).val();
+    // console.log($(this).val());
+  });
+});
+console.log ("テスト");
