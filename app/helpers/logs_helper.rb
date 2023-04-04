9b9dc1a75c44f557510491ed5d8b9997fe412302
aya <katoa5429@gmail.com>
2023-04-04 20:27:58 +0900

    ログの新規登録画面で目標終了予定日の計算式をヘルパーファイルに切り分け

--- app/helpers/logs_helper.rb
+++ app/helpers/logs_helper.rb
@@ -1,7 +1,6 @@
 module LogsHelper
   def culc_remain_page
     p "テスト"
-    p @studied_number
-    p params["study_id"]
+
   end
 end
--- app/helpers/studies_helper.rb
+++ app/helpers/studies_helper.rb
@@ -1,2 +1,24 @@
 module StudiesHelper
+  def calculate_study_total_number(study)
+    study_total_number = study.end_number - study.start_number
+  end
+
+  def calculate_study_total_days(study)
+    study_total_number = calculate_study_total_number(study)
+    if study_total_number % study.target_number == 0
+      study_total_days = study_total_number / study.target_number
+    else
+      study_total_days = (study_total_number.to_f / study.target_number.to_f).ceil
+    end
+  end
+
+  def calculate_target_day(study_total_days, dayOfWeek_arr, start_day)
+    while study_total_days >= 1
+      if (dayOfWeek_arr).include?(start_day.wday)
+        study_total_days -= 1
+      end
+      start_day += 1
+    end
+    return (start_day - 1).strftime('%Y年%m月%d日')
+  end
 end
