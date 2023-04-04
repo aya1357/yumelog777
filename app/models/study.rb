10f752cc235f2f68a2dc33865892e84bafbcb3c3
aya <katoa5429@gmail.com>
2023-04-04 11:26:54 +0900

    logコントローラーのlog_culc_apiのリファクタリング

--- app/models/study.rb
+++ app/models/study.rb
@@ -11,4 +11,36 @@ class Study < ApplicationRecord
   validates :start_day, presence: true
   validates :day_of_week, presence: true
   validates :status, inclusion: { in: [true, false] }
+
+  def total_study_number
+    end_number - start_number
+  end
+
+  def total_studied_number(user_id)
+    logs.where(user_id: user_id).pluck(:study_number).sum
+  end
+
+  def remaining_number(user_id, studied_pages)
+    remain_number = total_study_number - (total_studied_number(user_id) + studied_pages.to_i)
+    remain_number >= 0 ? remain_number : 0
+  end
+
+  def calculate_end_date(user_id, studied_pages)
+    today = Date.today
+    remaining_days = remaining_number(user_id, studied_pages)
+    end_day = remaining_days == 0 ? today.strftime('%Y年%m月%d日') : get_end_day(today, remaining_days)
+  end
+
+  def get_end_day(today, remaining_days)
+    day_of_week_arr = day_of_week.split(",").map(&:to_i).sort
+    remain_study_days = (remaining_days / target_number.to_f).ceil
+    while remain_study_days >= 1
+      if day_of_week_arr.include?(today.wday)
+        remain_study_days -= 1
+      end
+      today += 1
+    end
+    (today - 1).strftime('%Y年%m月%d日')
+  end
+
 end
