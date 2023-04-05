bac647f468ec26566ca632e192625d57050106cb
aya <katoa5429@gmail.com>
2023-04-05 13:24:47 +0900

    DBから読書したページ数を全取得、その合計を取得するメソッドをヘルパーに切り分け

--- app/helpers/studies_helper.rb
+++ app/helpers/studies_helper.rb
@@ -12,7 +12,12 @@ module StudiesHelper
     end
   end
 
-  def calculate_remain_study_days(study, total_studied_number) # 総読書日数を計算(読書したページ数を差し引いて計算)
+  def calculate_total_studied_number(study, current_user)
+    current_user.logs.where(study_id: study.id).pluck(:study_number).sum
+  end
+
+  def calculate_remain_study_days(study) # 総読書日数を計算(読書したページ数を差し引いて計算)
+    total_studied_number = calculate_total_studied_number(study, current_user)
     remain_number = calculate_study_total_number(study) - total_studied_number
     if remain_number % study.target_number == 0
       remain_study_days = remain_number / study.target_number
@@ -34,8 +39,9 @@ module StudiesHelper
     return (start_day - 1).strftime('%Y年%m月%d日')
   end
 
-  def automatic_end_day(study, remain_study_days)
+  def automatic_end_day(study)
     today = Date.today
+    remain_study_days = calculate_remain_study_days(study)
     dayOfWeek_arr = study.day_of_week.split(",").map(&:to_i).sort
     while remain_study_days >= 1
       if (dayOfWeek_arr).include?(today.wday)
