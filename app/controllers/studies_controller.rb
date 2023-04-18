800f87dd145e35ed3ef67ad0423890161ac61326
aya <katoa5429@gmail.com>
2023-04-18 15:32:07 +0900

    パス変更

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -16,7 +16,7 @@ class StudiesController < ApplicationController
   def create
     @study = current_user.studies.build(study_params)
     if @study.save
-      redirect_to studies_registration_complete_path(id: @study.id), success: t('defaults.message.registed', item: Study.model_name.human)
+      redirect_to calendars_path, success: t('defaults.message.registed', item: Study.model_name.human)
     else
       flash.now['danger'] = t('defaults.message.not_resisted', item: Study.model_name.human)
       render :new, status: :unprocessable_entity
