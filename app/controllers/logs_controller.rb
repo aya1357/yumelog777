1fcfd91434b653399b89b3ad30a80e05aacee5b9
aya <katoa5429@gmail.com>
2023-04-25 09:23:38 +0900

    モデルに切り分け、カードのコメントのランダム表示を新しくクラスを作成して作成

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -60,58 +60,8 @@ class LogsController < ApplicationController
     @log = Log.find(params[:id])
   end
 
-  def image_build
-    @log = Log.find(params[:id])
-
-    #背景画像の設定
-    image = MiniMagick::Image.open('./app/assets/images/frame.png')
-    image.resize "800x418"
-
-    #吹き出し画像の設定
-    speech_bubble = MiniMagick::Image.open('./app/assets/images/speech_bubble.png')
-    speech_bubble.resize "420x320"
-
-    #キャラクターの設定
-    character_files = Dir.glob('./app/assets/images/cards/*.png')
-    random_character = character_files.sample
-    character = MiniMagick::Image.open(random_character)
-    character.resize "310x200"
-
-    #テキストの設定
-    log_date = @log.log_date.strftime('%Y年%m月%d日')
-    total_read_pages = Log.where(log_date: @log.log_date).pluck(:study_number).sum
-
-    #キャラクターのコメントをランダム表示
-    comments = ["テスト", "test", "テステス", "testes", "ttt"]
-    random_comment = comments.sample
-
-    # 画像の合成
-    image = image.composite(character) do |c|
-      c.gravity "NorthWest"
-      c.geometry "+60+210"
-    end
-
-    image = image.composite(speech_bubble) do |c|
-      c.gravity "NorthWest"
-      c.geometry "+340+90"
-    end
-
-    image.combine_options do |c|
-      c.font "./app/assets/fonts/GenJyuuGothic-Bold.ttf"
-      c.pointsize "30"
-      c.gravity "NorthWest"
-      c.fill "black"
-      c.draw "text 130, 45'#{log_date}'"
-      c.draw "text 130, 100'総読書ページ数'"
-      c.draw "text 130, 140'#{total_read_pages}ページ'"
-      c.draw "text 450, 160'#{random_comment}'"
-    end
-    image.format 'png'
-  end
-
-
   def image
-    image = image_build
+    image = Log.character_card_img(params[:id])
     send_data image.to_blob, type: 'image/png', disposition: 'inline'
   end
 
