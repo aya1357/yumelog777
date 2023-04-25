3811907415dc6c7798cc76b687ae372216b8fe00
aya <katoa5429@gmail.com>
2023-04-25 16:37:41 +0900

    本の登録と読書記録の登録の使い方ガイドの出し分け機能を作成

--- app/assets/builds/application.js
+++ app/assets/builds/application.js
@@ -13107,6 +13107,39 @@
     });
   });
 
+  // app/javascript/packs/guide_change.js
+  document.addEventListener("turbo:load", function() {
+    $(".log_guide").hide();
+    $("#log_guide_btn").click(function() {
+      $(".log_guide").show();
+      $(".book_guide").hide();
+      $(this).removeClass(
+        "text-gray-600 px-6 block hover:text-blue-500 focus:outline-none"
+      ).addClass(
+        "text-gray-700 px-6 block hover:text-blue-500 focus:outline-none border-b-2 font-extrabold text-2xl border-blue-500"
+      );
+      $("#book_guide_btn").removeClass(
+        "text-gray-700 px-6 block hover:text-blue-500 focus:outline-none border-b-2 font-extrabold text-2xl border-blue-500"
+      ).addClass(
+        "text-gray-600 px-6 block hover:text-blue-500 focus:outline-none"
+      );
+    });
+    $("#book_guide_btn").click(function() {
+      $(".book_guide").show();
+      $(".log_guide").hide();
+      $(this).removeClass(
+        "text-gray-600 px-6 block hover:text-blue-500 focus:outline-none"
+      ).addClass(
+        "text-gray-700 px-6 block hover:text-blue-500 focus:outline-none border-b-2 font-extrabold text-2xl border-blue-500"
+      );
+      $("#log_guide_btn").removeClass(
+        "text-gray-700 px-6 block hover:text-blue-500 focus:outline-none border-b-2 font-extrabold text-2xl border-blue-500"
+      ).addClass(
+        "text-gray-600 px-6 block hover:text-blue-500 focus:outline-none"
+      );
+    });
+  });
+
   // app/javascript/application.js
   var import_jquery = __toESM(require_jquery());
   window.$ = window.jQuery = import_jquery.default;
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -9,5 +9,6 @@ import "./packs/calendars_studies_status.js"
 import "./packs/dayOfWeek_default.js"
 import "./packs/study_pages_comparison.js"
 import "./packs/top_animation.js"
+import "./packs/guide_change.js"
 import jquery from "jquery"
 window.$ = window.jQuery = jquery
