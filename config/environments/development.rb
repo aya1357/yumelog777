597f2ef1faea11c9c3b5feb102da0643f816a879
aya <katoa5429@gmail.com>
2023-05-04 00:34:36 +0900

    本番環境のパスワードリセット機能の不具合を修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,6 +73,19 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.action_mailer.delivery_method = :letter_opener_web
+  # config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :letter_opener_web
+  # SendGrid(メール送信用サーバー)の設定
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :domain => 'localhost',
+    :user_name => 'apikey',
+    :password => ENV['SENDGRID_API_KEY'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
